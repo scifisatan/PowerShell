d33251a8e5df118b4bbcc1b25624d174b5f8fb39
scifisatan <brawlershere@gmail.com>
2024-03-21 19:12:30 +0545

    Update PowerShell profile with new functions and modules

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,22 +1,27 @@
 $LSCustomFormatURL = "https://raw.githubusercontent.com/scifisatan/PowerShell/main/CustomLSFormat.format.ps1xml"
 $ModulePath = $env:PSModulePath.Split(';')[0]
 function cleanerls{
-    Get-ChildItem | Format-Wide -Column 1
+    Get-ChildItem | Format-Wide 
 }
 
 function fixFormatXML {
-    Invoke-RestMethod $LSCustomFormatUrl >  $ModulePath\Terminal-Icons\0.5.0\Terminal-Icons.format.ps1xml
+    curl $LSCustomFormatUrl >  $ModulePath\Terminal-Icons\0.5.0\Terminal-Icons.format.ps1xml
 }
 
 function installPoShFuck {
-    $tmp = (Join-Path $env:temp "poshfuck.zip")
-    $dst = (Join-Path $ModulePath PoShFuck)
+    $pfk = (Join-Path $env:temp "poshfuck.zip")
+    $dst = (Join-Path $env:PSModulePath.Split(';')[0] PoShFuck)
 
-    Invoke-WebRequest 'https://github.com/mattparkes/PoShFuck/archive/master.zip' -OutFile $tmp
-    
+    mkdir $dst -ea SilentlyContinue
+   
+    [Net.ServicePointManager]::SecurityProtocol = "tls12"
+    Invoke-WebRequest 'https://github.com/mattparkes/PoShFuck/archive/master.zip' -OutFile $pfk
+
+    $shell = New-Object -ComObject Shell.Application; $shell.Namespace($dst).copyhere(($shell.NameSpace($pfk)).items(),20)
+
     Move-Item "$dst\PoShFuck-master\*" "$dst" -Force
     Remove-Item "$dst\PoShFuck-master" -Recurse -Force
-    Remove-Item $tmp -Force
+    Remove-Item $pfk -Force
 
 }
 
@@ -25,16 +30,19 @@ function checkModule {
         [string]$ModuleName
     )
 
+    ##checking if it's installed or not
     if (!(Get-Module -ListAvailable -Name $ModuleName)) {
         Write-Host "Module $ModuleName is not installed"
+
         if ($ModuleName -eq "PoShFuck"){
             installPoShFuck
         }
 
-        Install-Module -Name $ModuleName -Force -SkipPublisherCheck
+        Install-Module -Name $ModuleName 
     }
 }
 
+# ------------------------ Main ------------------------
 $modules = @(
     "Terminal-Icons",
     "z",
@@ -42,14 +50,24 @@ $modules = @(
 )
 
 foreach ($module in $modules) {
-    checkModule $module
-    Import-Module $module
+    try {
+        Import-Module $module 
+    }
+    catch {
+        checkModule $module
+    }
+   
 }
 
-#custom format for terminal-icons in format-wide
 
-$ENV:STARSHIP_CONFIG = "C:\Users\Abi\terminalConfig.toml"
-Invoke-Expression (&starship init powershell)
+try {
+    $ENV:STARSHIP_CONFIG = "$ModulePath\..\terminalConfig.toml"
+    Invoke-Expression (&starship init powershell)
+}
+catch {
+    starship preset nerd-font-symbols -o $ModulePath\..\terminalConfig.toml
+}
+
 
 New-Alias -Name ls -Value cleanerls -Force
 New-Alias -Name fixls -Value fixFormatXML
